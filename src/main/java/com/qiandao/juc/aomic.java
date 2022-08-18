2540e093cefdf55c758052a9d612ecfa5a4f5f30
千岛 <notre1024@163.com>
2022-08-18 15:03:50 +0800

    001

--- src/main/java/com/qiandao/juc/aomic.java
+++ src/main/java/com/qiandao/juc/aomic.java
@@ -1,4 +1,21 @@
 package com.qiandao.juc;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class aomic {
+    public static void main(String[] args) {
+        new Thread(()->{
+            System.out.println("开启新线程"+Thread.currentThread().getName());
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            ReentrantLock lock = new ReentrantLock();
+            lock.lock();
+            System.out.println("加锁了。。。");
+            lock.unlock();
+        }).start();
+    }
 }
