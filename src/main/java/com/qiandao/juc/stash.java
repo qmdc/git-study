9e615930f7d9268421a5851a6ff4e9ede95c5b0b
千岛 <notre1024@163.com>
2022-08-18 17:20:29 +0800

    master切换更改

---
+++ src/main/java/com/qiandao/juc/stash.java
@@ -0,0 +1,8 @@
+package com.qiandao.juc;
+
+public class stash {
+    public static void main(String[] args) {
+        System.out.println("测试stash");
+        System.out.println("stash保存test2暂存区的代码");
+    }
+}
