baf0bc215fa046904c647894b6d472ee172975d4
千岛 <notre1024@163.com>
2022-08-18 19:05:38 +0800

    dev的临时开发

--- src/main/java/com/qiandao/juc/test01.java
+++ src/main/java/com/qiandao/juc/test01.java
@@ -3,5 +3,6 @@ package com.qiandao.juc;
 public class test01 {
     public static void main(String[] args) {
         System.out.println("test01推送分支");
+        System.out.println("dev的临时开发");
     }
 }
